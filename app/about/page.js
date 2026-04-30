05bde6a92963ffd2967e38e5a918a7eae8913650
سجاد محمد صادج <sijadmohamed00@gmail.com>
2026-04-30 19:48:56 +0300

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,7 @@
 export const dynamic = 'force-static';
+
 export const metadata = {
   title: "من نحن | IQR لإدارة المطاعم في العراق",
-  description: "تعرف على فريق IQR — الشركة الأولى لإدارة وتطوير المطاعم في العراق.",
 };
 
-// التأكد من استدعاء المكون الصحيح لصفحة About
 export { default } from "./AboutClient";
